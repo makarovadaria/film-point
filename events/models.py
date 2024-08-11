fe24dd2ab716b5e54362cd4d219e3b4079421b2f
Enrico <kallaste.enrix@gmail.com>
2024-08-11 08:48:14 +0300

    Add models and apps

--- events/models.py
+++ events/models.py
@@ -18,13 +18,3 @@ class Movies(models.Model):
 
     def __str__(self):
         return self.movie_name
-
-
-class MovieForm(forms.Form):
-    title = forms.CharField(max_length=128)
-    genre = forms.ModelChoiceField(queryset=Genre.objects.all())
-    rating = forms.IntegerField(min_value=1, max_value=10)
-    released = forms.DateField()
-    description = forms.CharField(widget=forms.Textarea, required=False)
-
-
