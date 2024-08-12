fde4f734b39554d4fe8e4aa2ee81682a33183f7a
Marianna <gilmarianna@hotmail.com>
2024-08-12 18:48:10 +0300

    create views and urls

---
+++ events/urls.py
@@ -0,0 +1,12 @@
+
+from django.urls import path
+
+from events import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('survey/', views.survey, name='survey'),
+    path('get_result/', views.get_result, name='get_result'),
+]
+
+    
--- events/views.py
+++ events/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    pass
+
+
+def survey(request):
+    pass
+
+
+def get_result(request):
+    pass
+
