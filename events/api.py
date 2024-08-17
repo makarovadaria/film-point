b73f7fcc98d913001d5ec781c50473e5156af57f
Enrico <kallaste.enrix@gmail.com>
2024-08-17 16:48:15 +0300

    add forms

--- events/api.py
+++ events/api.py
@@ -7,19 +7,17 @@ import requests
 
 from events.models import Movie
 
-
-
 API_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMTYxYzYxMmJjM2UyYjdlMWZiYzgxMjdmYWY0M2I0NiIsIm5iZiI6MTcyMzgwOTgwMC4wNDkxMTksInN1YiI6IjY2YmRkYjAyNTNhNTI1NTY4NmUxNDdiYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.c9YAGpSr1isIluCE77kzgro9UvlmIy0e-fRqqoPVkfM'
 API_URL = 'https://api.themoviedb.org/3'
 
-regions={}
+regions = {}
 regions["us"] = "US"
 regions["india"] = "IN"
 regions["europe"] = "FR"
 regions["asia"] = "JP"
 regions["latam"] = "BR"
 
-genres={}
+genres = {}
 genres["action"] = 28
 genres["comedy"] = 35
 genres["drama"] = 18
@@ -27,14 +25,11 @@ genres["horror"] = 27
 genres["romance"] = 10749
 genres["scifi"] = 878
 
-
 genres_file_path = os.path.join(settings.BASE_DIR, 'static/film-point/genres.json')
 with open(genres_file_path, 'r') as f:
     genres_data = json.load(f)
 
 GENRE_MAP = {genre['id']: genre['name'] for genre in genres_data['genres']}
-
-
 
 
 def get_film_list_mock(filter):
@@ -45,8 +40,8 @@ def get_film_list_mock(filter):
         objects_array = transform_movie_data(recommended_movies_json)
         return random.sample(objects_array, 10)
 
-def get_film_list_by_filter(filter):
 
+def get_film_list_by_filter(filter):
     base_url = f"{API_URL}/discover/movie?release_date.gte={filter['release_date_gte']}&release_date.lte={filter['release_date_lte']}&with_genres={genres[filter['genre']]}"
     if filter["region"] != "none":
         base_url += f"&region={regions[filter['region']]}"
@@ -69,12 +64,12 @@ def transform_movie_data(movie_data):
         image = data.get("poster_path", "")
         genre_ids = data.get("genre_ids", [])
         genre_names = [GENRE_MAP.get(genre_id, "Unknown") for genre_id in genre_ids]
-        genre =  ", ".join(map(str, genre_names))
+        genre = ", ".join(map(str, genre_names))
         year = data.get("release_date", "").split("-")[0]
         description = data.get("overview", "")
         rating = data.get("vote_average", 0.0)
 
-        movie = Movie(name=name, image=image, genre = genre, year=year, description=description, rating=rating)
+        movie = Movie(name=name, image=image, genre=genre, year=year, description=description, rating=rating)
         movies.append(movie)
 
     return movies
