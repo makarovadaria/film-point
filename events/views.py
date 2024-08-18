c1fe1c9a20e8446a7c6480c6a93e806a1d0f88ac
Enrico <kallaste.enrix@gmail.com>
2024-08-18 12:07:36 +0300

    add refactored survey

--- events/views.py
+++ events/views.py
@@ -1,9 +1,9 @@
-import random, requests # For making requests to TMDB
-from events.api import get_film_list_by_filter, get_film_list_mock
-from .models import UserAnswer, Movie
+import requests
+from events.api import get_film_list_by_filter
+from .models import Movie, SurveyQuestion
 from .forms import GenreForm, ReleaseDate, MovieRegion
-
 from django.shortcuts import redirect, render
+
 
 def get_movies(genre, year, region):
     url = f'https://api.themoviedb.org/3/discover/movie?api_key=YOUR_API_KEY&with_genres={genre}&primary_release_date.gte={year}-01-01&primary_release_date.lte={year}-12-31&with_original_language={region}'
@@ -12,113 +12,28 @@ def get_movies(genre, year, region):
         data = response.json()
         return data['results']
     else:
-        return [] # Handle error
-def survey1(request):
-    if request.method == 'POST':
-        form = GenreForm(request.POST)
-        if form.is_valid():
-            # Save user answer to database
-            user_answer = UserAnswer(
-                user_id=request.user.id,  # Replace with user authentication
-                question_type='genre',
-                answer=form.cleaned_data['genre']
-            )
-            user_answer.save()
-            return redirect('survey2')
-    else:
-        form = GenreForm()
-    return render(request, 'movie_survey1.html', {'form': form})
+        return []  # Handle error
 
-def survey2(request):
-    if request.method == 'POST':
-        form = ReleaseDate(request.POST)
-        if form.is_valid():
-            # Save user answer to database
-            user_answer = UserAnswer(
-                user_id=request.user.id,  # Replace with user authentication
-                question_type='year_range',
-                answer=form.cleaned_data['year_range']
-            )
-            user_answer.save()
-            return redirect('survey3')
-    else:
-        form = ReleaseDate()
-    return render(request, 'movie_survey2.html', {'form': form})
 
-def survey3(request):
+def intro_survey(request):
+    current_user = request.user
+    user_survey_stage = current_user
+    questions = SurveyQuestion.objects.all()
     if request.method == 'POST':
-        form = MovieRegion(request.POST)
-        if form.is_valid():
-            # Save user answer to database
-            user_answer = UserAnswer(
-                user_id=request.user.id,  # Replace with user authentication
-                question_type='region',
-                answer=form.cleaned_data['region']
-            )
-            user_answer.save()
-            return redirect('survey_result')
-    else:
-        form = MovieRegion()
-    return render(request, 'movie_survey3.html', {'form': form})
-# Similar views for survey3
+        answer = request.POST.get('answer')
 
-filter = {}
+    return render(request, 'events/movie_survey.html', {'questions': questions})
+
+
 def index(request):
     return render(request, 'events/index.html')
-def survey(request):
 
-    return render(request, 'events/movie_survey1.html')
-def survey_genre(request):
-    genre = request.GET.get('genres')
-    filter["genre"] = genre
-
-    return render(request, 'events/movie_survey2.html')
-def survey_year(request):
-    years = request.GET.get('years')
-    filter["years"] = years
-    return render(request, 'events/movie_survey3.html')
-def survey_region(request):
-    region = request.GET.get('region')
-    filter["region"] = region
-
-    years_str = filter.get("years", "")
-    if '-' in years_str:
-        start_year, end_year = map(str, years_str.split('-'))
-        filter["release_date_gte"] = start_year + "-01-01"
-        filter["release_date_lte"] = end_year + "-12-31"
-
-    if len(filter) < 4:
-        return render(request, 'events/index.html')
-
-    movie_list = get_film_list_by_filter(filter)
-    # movie_list = get_film_list_mock(filter)
-    return render(request, 'events/movie_survey_result.html', {'movie_list': movie_list})
 
 def recommendation(request):
     return render(request, 'events/recommendation.html')
 
 
-# def survey(request):
-#     if request.method == 'POST':
-#         form = MovieSurveyForm(request.POST)
-#         if form.is_valid():
-#             genre = form.cleaned_data['genre']
-#             moods = form.cleaned_data['moods']
-#             pace = form.cleaned_data['pace']
-#             age = form.cleaned_data['age_group']
-#
-#             recommended_movies = Movie.objects.filter(genre=genre, mood=moods, pace=pace, age_group=age)
-#             return render(request, '', )
-#
-#         else:
-#             form = MovieSurveyForm()
-#
-#         return render(request, '', {'form': form})
-
-
 def get_recommendations(request):
-
-
     # movie = get_object_or_404(id=movie_name)
     # recommended_movies = Movie.objects.filter(genre=movie.genre).exclude(id=movie.name)
 
--- film_point/settings.py
+++ film_point/settings.py
@@ -23,6 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
@@ -137,3 +138,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'events.User'
+
